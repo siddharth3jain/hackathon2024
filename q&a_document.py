742e0a682abee0c9f72ecddd6c7f5f672aa4a22c
Siddharth <siddharth3jain@gmail.com>
2024-03-17 16:03:45 +0530

    enabling fastapi

--- q&a_document.py
+++ q&a_document.py
@@ -1,6 +1,5 @@
 # The code snippet you provided is setting up a Gradio interface for a question-answering system. Here's a breakdown of what each part of the code is doing:
 from pathlib import Path
-from parameters import chroma_persist_directory
 from langchain_community.vectorstores import Chroma
 from initialization import embeddings_function
 from langchain.chains.question_answering import load_qa_chain
@@ -14,7 +13,7 @@ app = FastAPI()
 # load from disk
 # Load all pdf vectorstore
 # The line `db = Chroma(persist_directory=str(Path(chroma_persist_directory,'combined')), embedding_function=embeddings_function)` is creating an instance of the `Chroma` class with specific parameters. Here's a breakdown of what it's doing:
-db = Chroma(persist_directory=str(Path(chroma_persist_directory, 'combined')), embedding_function=embeddings_function)
+db = Chroma(persist_directory=str(Path('embeddings')), embedding_function=embeddings_function)
 
 
 def qa_response(files, query):
